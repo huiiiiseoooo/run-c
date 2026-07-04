8257d25842d42bc9b06cc2fc7408522bd629aa2c
huiiiiseoooo <sshs1552@naver.com>
2026-07-05 00:45:03 +0900

    chore: db연결 끊고 백엔드 서버 테스트 구문

--- backend/src/main/java/runc/jbnusw/JbnuswApplication.java
+++ backend/src/main/java/runc/jbnusw/JbnuswApplication.java
@@ -2,8 +2,10 @@ package runc.jbnusw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//데이터 베이스 연결안하고 테스트용 나중에 db연결할때 ()안에 있는 구문 지우고 실행
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class JbnuswApplication {
 
 	public static void main(String[] args) {
