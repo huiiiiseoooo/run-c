e6a1b59e7c5b5128c4a75d107f69ec8171a4498d
huiiiiseoooo <sshs1552@naver.com>
2026-07-05 00:27:05 +0900

    chore: Initial project setup (React + Spring Boot)

---
+++ backend/src/main/java/runc/jbnusw/JbnuswApplication.java
@@ -0,0 +1,13 @@
+package runc.jbnusw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbnuswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbnuswApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/runc/jbnusw/JbnuswApplicationTests.java
@@ -0,0 +1,13 @@
+package runc.jbnusw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbnuswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
